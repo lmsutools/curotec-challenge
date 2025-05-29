405504540238785e385f1a05ed84997204ba25e0
lmsutools <lcomunicamos3@gmail.com>
2025-05-29 11:58:30 -0500

    Setup Laravel Broadcasting (Pusher)

---
+++ routes/channels.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+
+/*
+|--------------------------------------------------------------------------
+| Broadcast Channels
+|--------------------------------------------------------------------------
+*/
+
+// Example:
+// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//     return (int) $user->id === (int) $id;
+// });
